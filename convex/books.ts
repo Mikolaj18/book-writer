b83e2ec0f4a8dc5d80b72af8a500e91f078c6fea
Mikołaj Urbański <m.urbanski@goldensubmarine.com>
2024-06-27 10:03:51 +0200

    Add userId to books table

--- convex/books.ts
+++ convex/books.ts
@@ -1,18 +1,29 @@
 import {mutation, query} from "./_generated/server";
-import {v} from "convex/values";
+import {ConvexError, v} from "convex/values";
 
 export const getBooks = query({
     async handler(ctx) {
-        return await ctx.db.query("books").collect()
-    }
+
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+        if(!userId) return [];
+
+        return await ctx.db.query("books")
+            .withIndex("by_tokenIdentifier", (q) => q.eq("tokenIdentifier", userId))
+            .collect();
+    },
 });
 export const createBook = mutation({
     args: {
         title: v.string(),
     },
     async handler(ctx, args) {
+
+        const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+        if(!userId) throw new ConvexError("Not authenticated");
+
         await ctx.db.insert("books", {
-            title: args.title
+            title: args.title,
+            tokenIdentifier: userId,
         });
     },
 });
